8455e591ab615d87576c062dde6174082b0b49e9
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2024-06-05 08:21:39 +0300

    feat: Dropdown, leave an example of using a  Dropdown

--- ui/dropdown.tsx
+++ ui/dropdown.tsx
@@ -1,18 +1,33 @@
 'use client';
 
-import { ButtonType, IconName, IconSize } from 'types';
+import { ReactNode, useState } from 'react';
 
+import { ButtonType, IconName, IconSize } from 'types';
 import { useDropdown } from 'helpers';
 
 import SvgIcon from './svg-icon';
 
-interface IDropdownProps {
-  label: string;
-  options: string[];
+interface IOption<T> {
+  typeId: string;
+  option: T; // FIXME:-- possibly you should change this type to an appropriate common type for option's arrays after creating these types into /types/components.ts
 }
 
-export default function Dropdown({ label, options }: IDropdownProps) {
+interface IDropdownProps<T> {
+  label: T; // FIXME:-- possibly you should change this type to an appropriate common type for labels after creating these types into /types/components.ts
+  options: IOption<T>[];
+}
+
+export default function Dropdown<T>({ label, options }: IDropdownProps<T>) {
+  const [selectedLabel, setSelectedLabel] = useState<T>(label);
+  const [isOptionSelected, setIsOptionSelected] = useState(false);
+
   const { isOpened, dropdownRef, toggleDropdown } = useDropdown();
+
+  const handleOptionClick = (option: T) => {
+    setSelectedLabel(option);
+    setIsOptionSelected(true);
+    toggleDropdown();
+  };
 
   return (
     <div
@@ -22,18 +37,30 @@ export default function Dropdown({ label, options }: IDropdownProps) {
       <button
         type={ButtonType.Button}
         onClick={toggleDropdown}
-        className='gradient-border flex w-full items-center justify-between rounded-lg p-2 text-sm hocus:border-transparent hocus:outline-none hocus:ring-[2.4px] hocus:ring-accentPrimary dark:bg-darkBase dark:text-whiteBase md:px-4 md:py-3 md:text-base lg:text-big'
+        className={`${isOptionSelected ? 'bg-accent-gradient border-transparent text-base font-bold text-whiteBase md:text-medium lg:text-lg' : 'gradient-border text-sm text-darkBase dark:bg-darkBase dark:text-whiteBase md:text-base lg:text-big'} flex w-full items-center justify-between rounded-lg p-2 hocus:border-transparent hocus:outline-none hocus:ring-[2.4px] hocus:ring-accentPrimary md:px-4 md:py-3`}
       >
-        {label}
+        {/* TODO--- border colour for dropdown's button: border-accentPrimary-darker */}
+        {selectedLabel as ReactNode}
         <SvgIcon
           id={IconName.Expand}
           size={{ width: IconSize.HalfM, height: IconSize.HalfM }}
-          className={`${isOpened ? 'rotate-180' : ''} transition-transform dark:fill-whiteBase`}
+          className={`${isOpened ? 'rotate-180' : ''} ${isOptionSelected ? 'fill-whiteBase' : 'dark:fill-whiteBase'} transition-transform`}
         />
       </button>
       {isOpened && (
-        <ul className='bg-background-gradient absolute z-20 w-full space-y-6 rounded-b-lg border-[2.4px] border-accentPrimary p-4 text-sm md:text-base lg:text-medium'>
-          {Array.isArray(options) && options.map((option) => <li key={option}>{option}</li>)}
+        <ul className='dark:bg-background-gradient absolute z-20 w-full space-y-6 rounded-b-lg border-[2.4px] border-accentPrimary bg-whiteBase p-4 text-sm md:text-base lg:text-medium'>
+          {Array.isArray(options) &&
+            options.map(({ typeId, option }) => (
+              <li key={typeId}>
+                <button
+                  type={ButtonType.Button}
+                  onClick={() => handleOptionClick(option)}
+                  className='hover:text-accentPrimary'
+                >
+                  {option as ReactNode}
+                </button>
+              </li>
+            ))}
         </ul>
       )}
     </div>
