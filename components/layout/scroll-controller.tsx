d7b24bbd73cb07c29b948c8152db96d73536485a
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2024-06-14 07:56:47 +0300

    fix: some fixes for ScrollController

--- components/layout/scroll-controller.tsx
+++ components/layout/scroll-controller.tsx
@@ -1,10 +1,18 @@
-// import { SvgIconUI } from 'ui';
+import { ButtonType, IconName, IconSize } from 'types';
+
+import { SvgIconUI } from 'ui';
 
 export default function ScrollController() {
   return (
-    <div className='absolute bottom-10 right-20 z-20'>
-      <p>ScrollController</p>
-      {/* <SvgIconUI /> */}
-    </div>
+    <button
+      type={ButtonType.Button}
+      className='fixed bottom-4 right-10 z-40 hidden size-10 items-center justify-center rounded-full border border-accentPrimary bg-whiteBase/10 md:flex lg:right-20 lg:size-16'
+    >
+      <SvgIconUI
+        id={IconName.Arrow}
+        size={{ width: IconSize.HalfM, height: IconSize.HalfM }}
+        className='fill-accentPrimary lg:size-9'
+      />
+    </button>
   );
 }
