32f9b453873861a0179a1f71bf3eb63bca1c4941
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2024-06-14 07:41:51 +0300

    feat: Hero markup and stylisation

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -8,7 +8,11 @@ export default function Hero() {
     <SectionTemplate
       title={SectionTitle.Hero}
       isBigTitle
+      titleStyle='md:w-[440px] lg:w-[550px]'
     >
+      <p className='mb-6 mt-4 text-medium md:mb-8 md:mt-6 md:w-[440px] md:text-xl lg:mb-16 lg:w-[458px] lg:text-2xl'>
+        Розпочніть шлях до успіху з нами — оформіть замовлення вже сьогодні
+      </p>
       <PrimaryButtonUI>Замовити</PrimaryButtonUI>
     </SectionTemplate>
   );
