f51042fd8f2b505d6edbd8f7371e1b6dbaac4ecf
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2024-06-14 13:27:19 +0300

    decompose: Overview

--- components/home/overview.tsx
+++ components/home/overview.tsx
@@ -1,9 +1,11 @@
-import { IconName, IconSize, SectionTitle } from 'types';
+import Image from 'next/image';
+
+import { IconName, SectionTitle } from 'types';
 
 import { idValues } from 'helpers';
+
 import { SectionTemplate } from 'template';
-import { SvgIconUI } from 'ui/index';
-import Image from 'next/image';
+import OverviewItem from './overview-item';
 
 import serviceOverview from '../../public/backgroundImage/service-overview.webp';
 
@@ -58,22 +60,11 @@ export default function ServiceOverview() {
         <ul className='space-y-6 max-md:bg-center max-md:px-2 md:w-[430px] md:space-y-8 md:bg-left lg:w-[631px] lg:space-y-12'>
           {Array.isArray(orderSteps) &&
             orderSteps.map(({ id, step, iconName }) => (
-              <li
+              <OverviewItem
                 key={id}
-                className='flex items-center gap-x-2 lg:gap-x-4'
-              >
-                <div className='flex items-center gap-x-4 md:gap-x-6 lg:gap-x-10'>
-                  <div className='relative flex h-[28px] w-[28px] items-center justify-center rounded-full bg-whiteBase/10 lg:h-10 lg:w-10'>
-                    <div className='absolute h-4 w-4 rounded-full bg-accentSecondary lg:h-6 lg:w-6'></div>
-                  </div>
-                  <SvgIconUI
-                    id={iconName}
-                    size={{ width: IconSize.L, height: IconSize.L }}
-                    className='lg:size-16'
-                  />
-                </div>
-                <p className='text-sm max-md:leading-130 md:text-base lg:text-big'>{step}</p>
-              </li>
+                step={step}
+                iconName={iconName}
+              />
             ))}
         </ul>
       </div>
