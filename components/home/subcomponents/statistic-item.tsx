99fc52bbf123463789c6b6fb55d93a94c848e833
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2024-06-17 09:07:51 +0300

    feat: add generalText style

--- components/home/subcomponents/statistic-item.tsx
+++ components/home/subcomponents/statistic-item.tsx
@@ -17,7 +17,7 @@ export default function StatisticItem({
 }: IStatisticItem) {
   return (
     <p
-      className={`flex flex-col items-center justify-center ${hideOnSmallScreen ? 'hidden md:flex' : ''} ${hideOnLargeScreen ? 'hidden max-lg:flex' : ''} ${showOnLargeScreen ? 'hidden lg:flex' : ''} md:text-base lg:text-big`}
+      className={`flex flex-col items-center justify-center ${hideOnSmallScreen ? 'hidden md:flex' : ''} ${hideOnLargeScreen ? 'hidden max-lg:flex' : ''} ${showOnLargeScreen ? 'hidden lg:flex' : ''} generalText`}
     >
       <strong className='bg-accent-gradient bg-clip-text text-6xl leading-130 tracking-[2px] text-transparent md:text-7xl lg:text-8xl'>
         {count}
--- helpers/tailwindPlugins.ts
+++ helpers/tailwindPlugins.ts
@@ -3,3 +3,21 @@ import plugin from 'tailwindcss/plugin';
 export const hocusFunction = plugin(function ({ addVariant }) {
   addVariant('hocus', ['&:hover', '&:focus']);
 });
+
+export const generalText = plugin(function ({ addComponents, theme }) {
+  addComponents({
+    '.generalText': {
+      fontSize: theme('fontSize.sm'),
+      lineHeight: theme('lineHeight.130'),
+
+      '@media (min-width: 768px)': {
+        fontSize: theme('fontSize.base'),
+        lineHeight: '1.5',
+      },
+
+      '@media (min-width: 1440px)': {
+        fontSize: theme('fontSize.big'),
+      },
+    },
+  });
+});
