c71db4f552737833bb19038adc627e6c7c086e59
SyroidNadiia <112964918+SyroidNadiia@users.noreply.github.com>
2024-06-23 14:53:44 +0300

    fix: add minHeight to SectionTemplate

--- components/FAQ/hero.tsx
+++ components/FAQ/hero.tsx
@@ -12,16 +12,15 @@ export default function Hero() {
       titleStyle='md:w-[356px] lg:w-[621px] lg:mt-[154px]'
       ctaStyle='md:w-[392px] lg:w-[570px] no-margin'
       ctaText={CtaText.FAQHero}
+      minHeight='lg:min-h-[792px]'
     >
-      <div>
       <Image
         src='/backgroundImage/faq-hero.webp'
         alt='Girl with a book and question marks symbolizing frequently asked questions and answers'
         width={192}
         height={208}
         className='max-md:mx-auto md:absolute md:right-10 md:top-1/2 md:h-[260px] md:w-[240px] md:-translate-y-1/2 lg:h-[584px] lg:w-[537px]'
-        />
-      </div>
+      />
     </SectionTemplate>
   );
 }
