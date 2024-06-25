a808984f5808f05ae4a94092347103d567d69ab5
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2024-06-25 22:47:25 +0300

    fix: state name isBlurred

--- components/calculation/theme-input.tsx
+++ components/calculation/theme-input.tsx
@@ -5,11 +5,11 @@ import { useState } from 'react';
 import { useCalculation } from 'context';
 
 export default function ThemeInput() {
-  const [isFocused, setIsFocused] = useState(false);
+  const [isBlurred, setIsBlurred] = useState(false);
 
   const { calculationData, handleThemeChange } = useCalculation();
 
-  const hasBackground = calculationData.theme !== '' && isFocused;
+  const hasBackground = calculationData.theme !== '' && isBlurred;
 
   return (
     <label htmlFor='theme'>
@@ -19,8 +19,8 @@ export default function ThemeInput() {
         value={calculationData.theme}
         className={`${hasBackground ? 'border-transparent bg-accent-gradient text-base font-bold text-whiteBase md:text-medium lg:text-lg' : 'border-accentPrimary-darker text-sm text-darkBase dark:bg-darkBase dark:text-whiteBase max-md:leading-130 md:text-base lg:text-big'} w-full rounded-lg border bg-whiteBase px-2 py-[11px] caret-accentPrimary placeholder:text-darkBase  placeholder:dark:text-whiteBase md:h-12 md:px-4`}
         placeholder='Введіть тему (не обов`язково)'
-        onFocus={() => setIsFocused(false)}
-        onBlur={() => setIsFocused(true)}
+        onFocus={() => setIsBlurred(false)}
+        onBlur={() => setIsBlurred(true)}
         onChange={handleThemeChange}
         autoComplete='off'
       />
