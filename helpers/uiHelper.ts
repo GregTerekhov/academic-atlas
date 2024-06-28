8b5879984d9ca983ef17958f264e94e95d10a475
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2024-06-28 21:34:50 +0300

    decompose: Dropdown styles

---
+++ helpers/uiHelper.ts
@@ -0,0 +1,9 @@
+export const getDropdownTriggerStyles = (isOptionSelected: boolean, isDropdownOpen: boolean) => {
+  return `${isOptionSelected ? 'border-none bg-accent-gradient' : 'border-accentPrimary-darker dark:bg-darkBase'} flex h-10 w-full items-center justify-between border border-solid px-2 hocus:border-transparent hocus:outline-none hocus:ring-[2px] hocus:ring-accentPrimary md:h-12 md:px-4 ${isDropdownOpen ? 'rounded-t-lg' : 'rounded-lg'}`;
+};
+
+export const getDropdownLabelStyles = (isOptionSelected: boolean) => {
+  return isOptionSelected
+    ? 'text-base font-bold text-whiteBase md:text-medium lg:text-lg'
+    : 'text-sm text-darkBase dark:text-whiteBase max-md:leading-130 md:text-base lg:text-big';
+};
