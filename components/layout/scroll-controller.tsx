eee3ad75ec9830e444a79db5c5c36a1c3b2c421f
SyroidNadiia <112964918+SyroidNadiia@users.noreply.github.com>
2024-06-28 21:42:12 +0300

    fix: scroll-to-top button styles and imports

--- components/layout/scroll-controller.tsx
+++ components/layout/scroll-controller.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useScrollController } from 'hooks/useScrollController';
+import { useScrollController } from 'hooks';
 import { ButtonType, IconName, IconSize } from 'types';
 
 import { SvgIconUI } from 'ui';
@@ -8,24 +8,20 @@ import { SvgIconUI } from 'ui';
 export default function ScrollController() {
   const { buttonRef, isVisible } = useScrollController();
 
-   const scrollToTop = () => {
-     window.scrollTo({
-       top: 0,
-       behavior: 'smooth',
-     });
-   };
-  
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <button
       ref={buttonRef}
       type={ButtonType.Button}
       aria-label='Scroll up button'
       onClick={scrollToTop}
-      className={`${
-        isVisible
-          ? 'flex translate-y-0 opacity-100'
-          : 'pointer-events-none flex translate-y-4 opacity-0'
-      } right-10 size-10 bottom-4 z-10 hidden items-center justify-center rounded-full border border-accentPrimary bg-whiteBase/10 transition duration-500 ease-out md:flex lg:right-20 lg:size-16`}
+      className={`fixed bottom-4 right-10 z-10 ${isVisible ? 'opacity-100 md:flex' : 'hidden opacity-0'} hidden size-10 items-center justify-center rounded-full border border-accentPrimary bg-whiteBase/10 transition-opacity lg:right-20 lg:size-16`}
     >
       <SvgIconUI
         id={IconName.Arrow}
