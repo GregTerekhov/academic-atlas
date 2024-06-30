8714116ba2911623b9a76a518e9865e8c89ea49a
Max3281 <113448092+Max3281@users.noreply.github.com>
2024-06-30 10:43:30 +0300

    add:PriceRender

--- components/calculation/price-result.tsx
+++ components/calculation/price-result.tsx
@@ -1,29 +1,36 @@
 'use client';
 
+import { useCalculation } from 'context/CalculationProvider';
+import { calculatePrice } from 'helpers/calculatePrice';
 import { ButtonType, CalculationTitle, IconName, IconSize, PrimaryButtonLabel } from 'types';
 
 import { PrimaryButtonUI, SvgIconUI } from 'ui';
 
 export default function PriceResult() {
-  // // Проміжковий приклад використання функції CalculatePrice
-  // const selectedWorkType = WorkType.Abstracts;
-  // const selectedExpertiseArea = ExpertiseArea.CultureAndArt;
-  // const selectedExecutionTime = ExecutionTime.LongTerm;
+  const { calculationData } = useCalculation();
 
-  // const finalPrice = calculatePrice(
-  //   selectedWorkType,
-  //   selectedExpertiseArea,
-  //   selectedExecutionTime,
-  //   90,
-  // );
-  // console.log(`Final Price: ${finalPrice}`);
+  const selectedWorkType = calculationData.workType;
+  const selectedExpertiseArea = calculationData.expertiseArea;
+  const selectedExecutionTime = calculationData.executionTime;
+  const selectedUniqueness = calculationData.uniqueness;
+
+  const calculatedPrice = calculatePrice(
+    selectedWorkType,
+    selectedExpertiseArea,
+    selectedExecutionTime,
+    selectedUniqueness,
+  );
+  const finalPrice = Math.round(calculatedPrice);
+
+  console.log(`Final Price: ${calculationData.expertiseArea}`);
+
   return (
     <>
       <h2 className='mb-8 !text-1.5xl text-darkBase dark:text-whiteBase md:mb-10 md:!text-3xl'>
         {CalculationTitle.CalculationResult}
       </h2>
       <p className='mb-8 text-center text-4xl text-whiteBase md:mb-10 md:text-5xl lg:text-7xl'>
-        від грн
+        від {finalPrice} грн
       </p>
       <p className='lg:text-bg mb-8 text-center text-sm text-whiteBase max-md:leading-130 md:mb-10 md:text-medium'>
         Для замовлення, зв’яжіться з нами у телеграм
