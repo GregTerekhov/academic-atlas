6dac13d91c1861264af0a0ef83e0fba9fd54a512
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2024-07-03 13:05:20 +0300

    fix: grids in Services section

--- components/home/services.tsx
+++ components/home/services.tsx
@@ -14,18 +14,16 @@ export default function Services() {
       title={SectionTitle.OurServices}
       id={Services ?? ''}
     >
-      {/* OLD_STYLES className='max-sm:space-y-4 sm:max-md:flex sm:max-md:flex-wrap sm:max-md:justify-between sm:max-md:gap-y-6 md:gap-6 md:max-lg:grid md:max-lg:grid-cols-4 md:max-lg:grid-rows-4 lg:flex lg:flex-wrap lg:gap-9'*/}
       <MappedListTemplate<IServiceItem>
         items={serviceItems}
-        className='max-md:space-y-4 md:grid md:grid-cols-4 md:grid-rows-4 md:gap-6 lg:gap-9'
+        className='max-md:space-y-4 md:grid md:grid-cols-3 md:grid-rows-3 md:gap-6 lg:gap-9'
       >
-        {({ id, imageSrc, imageAlt, serviceTitle, gridPosition }) => (
+        {({ id, imageSrc, imageAlt, serviceTitle }) => (
           <ServiceItem
             key={id}
             imageSrc={imageSrc}
             imageAlt={imageAlt}
             serviceTitle={serviceTitle}
-            gridPosition={gridPosition}
           />
         )}
       </MappedListTemplate>
