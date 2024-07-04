b5032a3ed2665df335fccdc7e0cbc13cfd721a65
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2024-07-04 03:05:59 +0300

    fix: max-height for sections

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -9,10 +9,12 @@ export default function NotFound() {
       isBigTitle
       title={SectionTitle.NotFound}
       titleStyle='bg-accent-gradient bg-clip-text text-monstrousSm text-transparent md:text-[160px] lg:text-monstrousLg text-center font-bold drop-shadow-[1px_3px_3px_#f8a401]'
+      minHeight='max-h-mobileSkeleton md:max-h-tabletSkeleton lg:max-h-desktopSkeleton h-screen flex flex-col items-center justify-center'
     >
       <h2 className='mb-6 flex flex-col md:mb-8 lg:mb-10'>
         <span>Ой!</span>
-        Схоже, ми не можемо знайти сторінку, яку ви шукаєте
+        Схоже, ми не можемо знайти сторінку,
+        <br className='hidden max-lg:inline' /> яку ви шукаєте
       </h2>
       <NotFoundNavigation />
     </SectionTemplate>
