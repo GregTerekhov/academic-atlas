6752e97e9ba5ea4221445c36fe2889cd5d00aec3
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2024-07-13 10:55:49 +0300

    fix: responsive and other fixes

--- ui/subcomponents/accordion-header.tsx
+++ ui/subcomponents/accordion-header.tsx
@@ -28,27 +28,31 @@ export default function AccordionHeader({
         onKeyDown(e);
       }}
     >
-      <SvgIcon
-        id={IconName.Question}
-        className='mx-auto mr-4 fill-accentPrimary dark:fill-accentSecondary md:mr-5 md:size-10'
-        size={{ width: IconSize.HalfL, height: IconSize.HalfL }}
-      />
+      <div className='mr-4 size-9 md:mr-5 md:size-10'>
+        <SvgIcon
+          id={IconName.Question}
+          className='mx-auto fill-accentPrimary dark:fill-accentSecondary md:size-10'
+          size={{ width: IconSize.HalfL, height: IconSize.HalfL }}
+        />
+      </div>
       <h2
-        className={`mr-2 flex-1 text-left group-hover:bg-accent-lightGradient group-hover:bg-clip-text group-hover:text-transparent dark:group-hover:bg-accent-darkGradient ${
+        className={`mr-2 flex-1 text-left group-hover:bg-accent-lightGradient group-hover:bg-clip-text group-hover:text-transparent dark:group-hover:bg-accent-darkGradient max-sm:text-medium ${
           isOpen ? 'bg-none text-accentPrimary dark:text-accentSecondary' : ''
         }`}
       >
         {title}
       </h2>
-      <SvgIcon
-        id={IconName.Expand}
-        className={`mx-auto transition-transform duration-200 group-hover:fill-accentPrimary-darker dark:group-hover:fill-accentSecondary-darker md:size-8 ${
-          isOpen
-            ? 'rotate-180 transform fill-accentPrimary dark:fill-accentSecondary'
-            : 'fill-whiteBase'
-        }`}
-        size={{ width: IconSize.HalfM, height: IconSize.HalfM }}
-      />
+      <div className='size-6 md:size-8'>
+        <SvgIcon
+          id={IconName.Expand}
+          className={`mx-auto transition-transform duration-200 group-hover:fill-accentPrimary-darker dark:group-hover:fill-accentSecondary-darker md:size-8 ${
+            isOpen
+              ? 'rotate-180 transform fill-accentPrimary dark:fill-accentSecondary'
+              : 'fill-darkBase dark:fill-whiteBase'
+          }`}
+          size={{ width: IconSize.HalfM, height: IconSize.HalfM }}
+        />
+      </div>
     </div>
   );
 }
