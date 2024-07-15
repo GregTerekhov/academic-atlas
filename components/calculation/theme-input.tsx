967210f4ae4923e0af1c2038e19f2a2a3efabadc
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2024-07-15 03:58:35 +0300

    fix: main background and add aria-label for theme input

--- components/calculation/theme-input.tsx
+++ components/calculation/theme-input.tsx
@@ -16,6 +16,7 @@ export default function ThemeInput() {
       <input
         type='text'
         id='theme'
+        aria-label='Enter a theme of your work'
         value={calculationData.theme}
         className={`${hasBackground ? 'border-transparent bg-accent-lightGradient text-base font-bold text-whiteBase dark:bg-accent-darkGradient md:text-medium lg:text-lg' : 'border-accentPrimary text-sm text-darkBase dark:border-accentSecondary-darker dark:bg-darkBase dark:text-whiteBase max-md:leading-130 md:text-base lg:text-big'} h-10 w-full rounded-lg border bg-whiteBase px-2 py-[11px] caret-accentPrimary placeholder-shown:text-darkBase focus:outline-transparent focus:ring-[2px] focus:ring-accentPrimary dark:caret-accentSecondary placeholder-shown:dark:text-whiteBase dark:focus:ring-accentSecondary md:h-12 md:px-4`}
         placeholder='Введіть тему (не обов`язково)'
