8376da410b31ae64bc939466b9f1ba1cfe5ae02a
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2024-07-17 17:50:02 +0300

    fix: other fixes

--- helpers/uiHelper.ts
+++ helpers/uiHelper.ts
@@ -1,5 +1,10 @@
 export const getDropdownTriggerStyles = (isOptionSelected: boolean, isDropdownOpen: boolean) => {
-  return `${isOptionSelected ? 'border-none bg-accent-lightGradient dark:bg-accent-darkGradient' : 'bg-none border-accentPrimary dark:border-accentSecondary-darker bg-whiteBase dark:bg-darkBase border'} flex h-10 w-full items-center justify-between border-solid px-2 hover:border-transparent dark:hover:border-transparent hocus:outline-none hocus:ring-[2px] hocus:ring-accentPrimary dark:hocus:ring-accentSecondary md:h-12 md:px-4 ${isDropdownOpen ? 'rounded-t-lg' : 'rounded-lg'}`;
+  const toggledSelectedStyles = isOptionSelected
+    ? 'border-none bg-accent-lightGradient dark:bg-accent-darkGradient'
+    : 'bg-none border-accentPrimary dark:border-accentSecondary-darker bg-whiteBase dark:bg-darkBase border';
+  const toggledOpenedStyles = isDropdownOpen ? 'rounded-t-lg' : 'rounded-lg';
+
+  return `${toggledSelectedStyles} ${toggledOpenedStyles} flex h-10 w-full items-center justify-between border-solid px-2 hover:border-transparent dark:hover:border-transparent hocus:outline-none hocus:ring-[2px] hocus:ring-accentPrimary dark:hocus:ring-accentSecondary md:h-12 md:px-4`;
 };
 
 export const getDropdownLabelStyles = (isOptionSelected: boolean) => {
