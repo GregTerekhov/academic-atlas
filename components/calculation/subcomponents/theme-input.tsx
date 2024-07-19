6572d62cae70ba0eb0f23fd214cf1eb2d2d6b524
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2024-07-20 02:21:31 +0300

    fix: RangeInput and ThemeInput styles

--- components/calculation/subcomponents/theme-input.tsx
+++ components/calculation/subcomponents/theme-input.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react';
 
 import { useCalculation } from 'context';
+import { AriaDescriptionUI } from 'ui';
 
 export default function ThemeInput() {
   const [isBlurred, setIsBlurred] = useState(false);
@@ -16,16 +17,20 @@ export default function ThemeInput() {
       <input
         type='text'
         id='theme'
-        aria-label='Enter a theme of your work'
         value={calculationData.theme}
-        className={`${hasBackground ? 'border-transparent bg-accent-lightGradient text-base font-bold text-whiteBase dark:bg-accent-darkGradient md:text-medium lg:text-lg' : 'border-accentPrimary text-sm text-darkBase dark:border-accentSecondary-darker dark:bg-darkBase dark:text-whiteBase max-md:leading-130 md:text-base lg:text-big'} h-10 w-full rounded-lg border bg-whiteBase px-2 py-[11px] caret-accentPrimary placeholder-shown:text-darkBase focus:outline-transparent focus:ring-[2px] focus:ring-accentPrimary dark:caret-accentSecondary placeholder-shown:dark:text-whiteBase dark:focus:ring-accentSecondary md:h-12 md:px-4`}
+        className={`h-10 w-full rounded-lg border bg-whiteBase px-2 py-[11px] caret-accentPrimary placeholder-shown:text-darkBase focus:outline-transparent focus:ring-[2px] focus:ring-accentPrimary dark:caret-accentSecondary placeholder-shown:dark:text-whiteBase dark:focus:ring-accentSecondary md:h-12 md:px-4 ${hasBackground ? 'border-transparent bg-accent-lightGradient text-base font-bold text-whiteBase dark:bg-accent-darkGradient md:text-medium lg:text-lg' : 'border-accentPrimary text-sm text-darkBase dark:border-accentSecondary-darker dark:bg-darkBase dark:text-whiteBase max-md:leading-130 md:text-base lg:text-big'}`}
         placeholder='Введіть тему (не обов`язково)'
         onFocus={() => setIsBlurred(false)}
         onBlur={() => setIsBlurred(true)}
         onChange={handleThemeChange}
         autoComplete='off'
+        aria-describedby='theme-input'
       />
-      <span className='sr-only'>Enter your work theme</span>
+      <AriaDescriptionUI
+        id='theme-input'
+        description='В цьому полі ви можете ввести тему вашої роботи. Зверніть увагу, що це поле не є
+        обов`язковим для розрахунку вартості вашої роботи'
+      />
     </label>
   );
 }
