89b6271495ce2946e3c242b9599794e3cdb16379
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2024-07-22 12:52:27 +0300

    fix: LegacyLink style

--- components/layout/policy-link.tsx
+++ components/layout/policy-link.tsx
@@ -8,13 +8,15 @@ import { MenuLinks, Paths } from 'types';
 export default function LegacyLink() {
   const pathname = usePathname();
   return (
-    <Link
-      href={Paths.LegalInfo}
-      className='mx-auto block text-center text-xs hocus:text-accentPrimary md:text-sm lg:text-base'
-      aria-label='Посилання переходу на сторінку Політики конфіденційності та умов використання'
-      aria-current={pathname === Paths.LegalInfo ? 'page' : undefined}
-    >
-      {MenuLinks.LegalInfo}
-    </Link>
+    <p className='text-center'>
+      <Link
+        href={Paths.LegalInfo}
+        className='text-xs hocus:text-accentPrimary md:text-sm lg:text-base'
+        aria-label='Посилання переходу на сторінку Політики конфіденційності та умов використання'
+        aria-current={pathname === Paths.LegalInfo ? 'page' : undefined}
+      >
+        {MenuLinks.LegalInfo}
+      </Link>
+    </p>
   );
 }
