35026d26cd832387dac0eb10d7c1e59d5c1a9849
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2024-07-22 03:24:31 +0300

    fix: scroll body on Safari mobile browser

--- template/mobile-menu.tsx
+++ template/mobile-menu.tsx
@@ -1,4 +1,5 @@
 import { Container } from 'layout';
+import { RemoveScroll } from 'react-remove-scroll';
 
 interface IMobileMenuProps {
   children: React.ReactNode;
@@ -7,12 +8,17 @@ interface IMobileMenuProps {
 
 export default function MobileMenu({ children, isOpen }: Readonly<IMobileMenuProps>) {
   return (
-    <div
-      className={`fixed top-20 z-50 h-screen max-h-mobileMenu w-screen overflow-auto bg-whiteBase bg-background-light-gradient pb-20 pt-10 transition-[left] duration-500 before:fixed before:left-0 before:top-0 before:z-40 before:w-full before:content-[""] dark:bg-background-dark-gradient md:top-24 md:max-h-tabletMenu ${
-        isOpen ? 'left-0' : '-left-full'
-      }`}
+    <RemoveScroll
+      forwardProps
+      enabled={isOpen}
     >
-      <Container>{children}</Container>
-    </div>
+      <div
+        className={`scroll fixed top-20 z-50 h-screen max-h-mobileMenu w-screen overflow-auto bg-whiteBase bg-background-light-gradient pb-20 pt-10 transition-[left] duration-500 before:fixed before:left-0 before:top-0 before:z-40 before:w-full before:content-[""] dark:bg-background-dark-gradient md:top-24 md:max-h-tabletMenu ${
+          isOpen ? 'left-0' : '-left-full'
+        }`}
+      >
+        <Container>{children}</Container>
+      </div>
+    </RemoveScroll>
   );
 }
