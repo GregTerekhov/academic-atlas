45f7c4394efc3e93dd42a6cfbd2a80ec8c786e71
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2024-07-22 12:44:58 +0300

    fix: ThemeInput styles when it is focusing

--- helpers/calculationHelper.ts
+++ helpers/calculationHelper.ts
@@ -194,7 +194,7 @@ export const getThemeInputStyles = (hasBackground: boolean) => {
     ? 'border-none bg-accent-lightGradient text-base font-bold text-whiteBase dark:bg-accent-darkGradient md:text-medium lg:text-lg'
     : 'border-accentPrimary text-sm text-darkBase dark:border-accentSecondary-darker dark:bg-darkBase dark:text-whiteBase max-md:leading-130 md:text-base lg:text-big';
 
-  return `h-10 w-full rounded-lg border bg-whiteBase px-2 py-[11px] caret-accentPrimary placeholder-shown:text-darkBase focus:outline-transparent focus:ring-[2px] focus:ring-accentPrimary dark:caret-accentSecondary placeholder-shown:dark:text-whiteBase dark:focus:ring-accentSecondary md:h-12 md:px-4 ${hasThemeValueClass}`;
+  return `h-10 w-full rounded-lg border bg-whiteBase px-2 py-[11px] caret-accentPrimary placeholder-shown:text-darkBase focus:outline-none focus:border-none focus:ring-[2px] focus:ring-accentPrimary dark:caret-accentSecondary placeholder-shown:dark:text-whiteBase dark:focus:ring-accentSecondary md:h-12 md:px-4 ${hasThemeValueClass}`;
 };
 
 export const getCheckboxStyles = (isChecked: boolean) => {
