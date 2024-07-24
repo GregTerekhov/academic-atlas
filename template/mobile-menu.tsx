7d3d1a79ca46fafcc611cad8a809220bfcf2a21c
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2024-07-24 13:30:35 +0300

    fix: position menu on small devices

--- template/mobile-menu.tsx
+++ template/mobile-menu.tsx
@@ -14,7 +14,7 @@ export default function MobileMenu({ children, isOpen }: Readonly<IMobileMenuPro
       enabled={isOpen}
     >
       <div
-        className={`scroll fixed top-20 z-50 h-screen max-h-mobileMenu w-screen overflow-auto bg-whiteBase bg-background-light-gradient pb-20 pt-10 transition-[left] duration-500 before:fixed before:left-0 before:top-0 before:z-40 before:w-full before:content-[""] dark:bg-background-dark-gradient md:top-24 md:max-h-tabletMenu ${
+        className={`scroll fixed top-20 z-50 h-full  w-screen overflow-auto bg-whiteBase bg-background-light-gradient pb-52 pt-10 transition-[left] duration-500 before:fixed before:left-0 before:top-0 before:w-full before:content-[""] dark:bg-background-dark-gradient md:top-24 md:max-h-tabletMenu ${
           isOpen ? 'left-0' : '-left-full'
         }`}
       >
