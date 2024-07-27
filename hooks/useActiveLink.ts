1502703586ddd4a21a487d25b7237420b51b1518
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2024-07-27 21:28:00 +0300

    fix: navigation

--- hooks/useActiveLink.ts
+++ hooks/useActiveLink.ts
@@ -1,53 +1,54 @@
 'use client';
 
-import { useEffect, useRef, useState } from 'react';
-import { usePathname } from 'next/navigation';
+import { useCallback, useEffect, useState } from 'react';
+import { usePathname, useRouter } from 'next/navigation';
 import { debounce } from 'lodash';
 
-import { Paths } from '../types';
-import { getAdaptedLinks } from '../helpers';
+import { MenuLinks, Paths } from '../types';
 import { useIntersectionObserver } from './useIntersectionObserver';
+import { useInitialiseSection } from './useInitialiseSection';
 
 export const useActiveLink = (isDesktop: boolean) => {
   const pathname = usePathname();
+  const router = useRouter();
 
-  const initialLink = pathname || `${window.location.hash}`;
+  const [activeLink, setActiveLink] = useState<string>(pathname);
 
-  const [activeLink, setActiveLink] = useState<string>(initialLink);
-  const sections = useRef<{ id: string; path: string }[]>([]);
-  const sectionRefs = useRef<Element[]>([]);
+  const { sections, sectionRefs, initialiseSections } = useInitialiseSection(isDesktop);
+
+  const updateActiveLink = useCallback(() => {
+    const legalPagesPaths = pathname !== Paths.Offer && pathname !== Paths.Policy;
+
+    if (legalPagesPaths) {
+      const hash = window.location.hash;
+
+      if (hash) {
+        setActiveLink(hash);
+      } else {
+        setActiveLink(pathname as Paths);
+      }
+    } else {
+      setActiveLink('');
+    }
+  }, [pathname]);
 
   useEffect(() => {
-    sectionRefs.current = Array.from(document.querySelectorAll('section[id]'));
-    const adaptedLinks = getAdaptedLinks(isDesktop);
-    sections.current = adaptedLinks.map(({ path, id }) => {
-      return { id: id ?? '', path };
-    });
-
-    const updateActiveLink = () => {
-      setActiveLink(pathname);
-      // setActiveLink(window.location.pathname + window.location.hash);
-    };
-
+    initialiseSections();
     updateActiveLink();
 
     const handleScroll = debounce(() => {
-      if (window.scrollY === 0) {
+      if (window.scrollY === 0 && pathname === Paths.Main) {
         setActiveLink(pathname as Paths);
         window.history.pushState(null, '', pathname);
       }
     }, 100);
 
     window.addEventListener('scroll', handleScroll);
-    // window.addEventListener('hashchange', updateActiveLink); //FIXME: --- if all right, delete after texting
-    // window.addEventListener('popstate', updateActiveLink); //FIXME: --- if all right, delete after texting
 
     return () => {
       window.removeEventListener('scroll', handleScroll);
-      //   window.removeEventListener('hashchange', updateActiveLink); //FIXME: --- if all right, delete after texting
-      //   window.removeEventListener('popstate', updateActiveLink); //FIXME: --- if all right, delete after texting
     };
-  }, [pathname, isDesktop]);
+  }, [initialiseSections, pathname, updateActiveLink]);
 
   const handleIntersection = debounce((entries: IntersectionObserverEntry[]) => {
     entries.forEach((entry) => {
@@ -61,9 +62,37 @@ export const useActiveLink = (isDesktop: boolean) => {
         }
       }
     });
-  }, 800);
+  }, 400);
 
   useIntersectionObserver(sectionRefs.current, { root: null, threshold: 0.5 }, handleIntersection);
 
-  return { activeLink, setActiveLink };
+  const handleLinkClick = (
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+    label: MenuLinks,
+    path: Paths,
+  ) => {
+    const isMainSection = [MenuLinks.Services, MenuLinks.AboutUs, MenuLinks.Feedback].includes(
+      label,
+    );
+
+    if (pathname === Paths.Main) {
+      if (label === MenuLinks.Main) {
+        e.preventDefault();
+
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth',
+        });
+
+        setActiveLink(Paths.Main);
+        router.push(Paths.Main);
+      } else if (isMainSection && activeLink !== path) {
+        setActiveLink(path);
+      }
+    } else {
+      setActiveLink(path);
+    }
+  };
+
+  return { activeLink, handleLinkClick };
 };
