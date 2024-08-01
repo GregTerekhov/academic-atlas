2774c0aec31552dc73f3a5437a5d507a0d784d7a
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2024-08-01 13:12:14 +0300

    fix: body jump when menu is opened

--- helpers/toggleScrollLock.ts
+++ helpers/toggleScrollLock.ts
@@ -1,12 +1,17 @@
 export const toggleScrollLock = (isLocked: boolean) => {
   const root = document.firstElementChild;
   const body = document.body;
+  const scrollPosition = window.scrollY;
 
   if (isLocked) {
+    body.style.top = `-${scrollPosition}px`;
     body.classList.add('no-scroll');
     root?.classList.add('no-scroll');
   } else {
     body.classList.remove('no-scroll');
     root?.classList.remove('no-scroll');
+    const scrollY = body.style.top ? parseInt(body.style.top, 10) * -1 : 0;
+    body.style.top = '';
+    window.scrollTo(0, scrollY);
   }
 };
