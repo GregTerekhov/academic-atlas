3f36835d17c45df29407371d0094fcf668a6d163
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2024-08-01 13:13:32 +0300

    fix: delete unused function

--- helpers/notFoundHelper.ts
+++
@@ -1,7 +0,0 @@
-import { Paths } from '../types';
-
-export const isValidHashPaths = (hash: string) => {
-  const hashPaths = Object.values(Paths).filter((path) => path.startsWith('/#'));
-
-  return hashPaths.includes(hash as Paths);
-};
