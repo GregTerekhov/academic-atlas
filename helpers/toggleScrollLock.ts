0691ca9b028dab3c169e860da984846fbed78338
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2024-08-06 01:58:17 +0300

    fix: jump of body to right after opening burger menu

--- helpers/toggleScrollLock.ts
+++ helpers/toggleScrollLock.ts
@@ -1,3 +1,28 @@
+const getScrollBarWidth = () => {
+  const scrollDiv = document.createElement('div');
+  scrollDiv.style.visibility = 'hidden';
+  scrollDiv.style.width = '100px';
+
+  document.body.appendChild(scrollDiv);
+
+  const widthNoScroll = scrollDiv.offsetWidth;
+  scrollDiv.style.overflow = 'scroll';
+
+  const switcherDiv = document.createElement('div');
+  switcherDiv.style.width = '100%';
+  scrollDiv.appendChild(switcherDiv);
+
+  const widthWithScroll = switcherDiv.offsetWidth;
+
+  scrollDiv.parentNode?.removeChild(scrollDiv);
+
+  return widthNoScroll - widthWithScroll;
+};
+
+const hasScrollbar = () => {
+  return document.body.scrollHeight > document.body.clientHeight;
+};
+
 export const toggleScrollLock = (isLocked: boolean) => {
   const root = document.firstElementChild;
   const body = document.body;
@@ -7,6 +32,11 @@ export const toggleScrollLock = (isLocked: boolean) => {
     body.style.top = `-${scrollPosition}px`;
     body.classList.add('no-scroll');
     root?.classList.add('no-scroll');
+    if (hasScrollbar()) {
+      body.style.width = `calc(100% - ${getScrollBarWidth()}px)`;
+    } else {
+      body.style.width = '100%';
+    }
   } else {
     body.classList.remove('no-scroll');
     root?.classList.remove('no-scroll');
