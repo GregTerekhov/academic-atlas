d0294b91bfa44773ad989ed797054cc646b0984f
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2024-08-06 16:09:53 +0300

    fix: return to previous variant

--- types/layoutTypes.ts
+++ types/layoutTypes.ts
@@ -163,7 +163,7 @@ export enum OpenGraphType {
 }
 
 export enum OpenGraphURL {
-  HOME = 'https://academic-atlas-git-draft-add-og-images-gregterekhovs-projects.vercel.app', //FIXME: --- add real path
+  HOME = 'https://AcademicAtlas.com', //FIXME: --- add real path
   FAQ = 'https://AcademicAtlas.com/faq', //FIXME: --- add real path,
   PARTNERSHIP = 'https://AcademicAtlas.com/partnership', //FIXME: --- add real path,
   POLICY = 'https://AcademicAtlas.com/legal', //FIXME: --- add real path,
@@ -172,9 +172,9 @@ export enum OpenGraphURL {
 }
 
 export enum OpenGraphImageURL {
-  HOME = 'https://academic-atlas-git-draft-add-og-images-gregterekhovs-projects.vercel.app/images/og-image.jpg', //FIXME: --- add image,
+  HOME = 'https://AcademicAtlas.com/images/og-image.jpg', //FIXME: --- add image,
   FAQ = 'https://AcademicAtlas.com/images/faq-og-image.jpg', //FIXME: --- add image,
-  PARTNERSHIP = '/images/partnership-og-image.jpg', //FIXME: --- add image,
+  PARTNERSHIP = 'https://AcademicAtlas.com/images/partnership-og-image.jpg', //FIXME: --- add image,
   POLICY = 'https://AcademicAtlas.com/images/legal-og-image.jpg', //FIXME: --- add image,
   NOT_FOUND = 'https://AcademicAtlas.com/images/404-og-image.jpg', //FIXME: --- add image,
   OFFER = 'https://AcademicAtlas.com/images/offer-og-image.jpg', //FIXME: --- add image,
