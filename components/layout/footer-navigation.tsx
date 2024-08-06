3f6f8a006c2bf14592dcca2759de5b1f84e715f2
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2024-08-06 04:43:10 +0300

    fix: getMenuAriaCurrent value for header and footer menu

--- components/layout/footer-navigation.tsx
+++ components/layout/footer-navigation.tsx
@@ -4,23 +4,13 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 import { AriaLabel, Paths, PositionInLayout } from 'types';
-import { getFooterLinks, mapArray } from 'helpers';
+import { getFooterLinks, getMenuAriaCurrent, mapArray } from 'helpers';
 
 import { CalculationLinkDesktop, CalculationLinkMobile } from './subcomponents';
 
 export default function FooterMenu() {
   const pathname = usePathname();
   const footerMenuLinks = getFooterLinks();
-
-  const getAriaCurrent = (path: Paths) => {
-    if (pathname === path) {
-      return 'page';
-    } else if (pathname.startsWith('#')) {
-      return 'location';
-    } else {
-      return undefined;
-    }
-  };
 
   return (
     <nav aria-label={AriaLabel.FooterNav}>
@@ -36,7 +26,7 @@ export default function FooterMenu() {
           <li key={label}>
             <Link
               href={path}
-              aria-current={getAriaCurrent(path)}
+              aria-current={getMenuAriaCurrent(path, pathname)}
               className='generalText hocus:text-accentPrimary dark:hocus:text-accentSecondary'
             >
               {label}
