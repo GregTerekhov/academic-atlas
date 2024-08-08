36b49a513a7ad7ad51ee4c32b4e19614f983b201
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2024-08-08 14:11:28 +0300

    draft: fix burger-menu scroll and add draft for navigation, and some small fixes

--- components/layout/menu.tsx
+++ components/layout/menu.tsx
@@ -2,12 +2,15 @@ import { PositionInLayout } from 'types';
 
 import Contacts from './contacts';
 import { Navigation } from './subcomponents';
+// import { Suspense } from 'react';
 
 export default function Menu() {
   return (
     <>
       <div className='mx-auto hidden space-y-12 max-lg:block max-md:max-w-fit md:max-lg:pl-[120px]'>
+        {/* <Suspense fallback={null}> */}
         <Navigation />
+        {/* </Suspense> */}
         <Contacts variant={PositionInLayout.Header} />
       </div>
       <div className='hidden lg:block'>
--- data/metadata.ts
+++ data/metadata.ts
@@ -5,7 +5,6 @@ import {
   OpenGraphImageAlt,
   OpenGraphImageURL,
   OpenGraphType,
-  OpenGraphURL,
 } from '../types';
 
 const OPEN_GRAPH_IMAGE_WIDTH = 1200;
@@ -19,7 +18,7 @@ export const MetadataTexts = {
     openGraph: {
       title: MetadataTitle.HOME,
       description: MetadataDescription.HOME,
-      //   url: OpenGraphURL.HOME,
+      //   url: OpenGraphURL.HOME, //FIXME: uncomment
       type: OpenGraphType.Website,
       images: [
         {
@@ -38,11 +37,11 @@ export const MetadataTexts = {
     openGraph: {
       title: MetadataTitle.FAQ,
       description: MetadataDescription.FAQ,
-      //   url: OpenGraphURL.FAQ,
+      //   url: OpenGraphURL.FAQ, //FIXME: uncomment
       type: OpenGraphType.Article,
       images: [
         {
-          //   url: OpenGraphImageURL.FAQ,
+          url: OpenGraphImageURL.FAQ,
           width: OPEN_GRAPH_IMAGE_WIDTH,
           height: OPEN_GRAPH_IMAGE_HEIGHT,
           alt: OpenGraphImageAlt.FAQ,
@@ -57,7 +56,7 @@ export const MetadataTexts = {
     openGraph: {
       title: MetadataTitle.PARTNERSHIP,
       description: MetadataDescription.PARTNERSHIP,
-      url: OpenGraphURL.PARTNERSHIP,
+      // url: OpenGraphURL.PARTNERSHIP, //FIXME: uncomment
       type: OpenGraphType.Article,
       images: [
         {
@@ -73,57 +72,57 @@ export const MetadataTexts = {
     title: MetadataTitle.POLICY,
     description: MetadataDescription.POLICY,
     keywords: MetadataKeywords.POLICY,
-    openGraph: {
-      title: MetadataTitle.POLICY,
-      description: MetadataDescription.POLICY,
-      //   url: OpenGraphURL.POLICY,
-      type: OpenGraphType.Article,
-      images: [
-        {
-          //   url: OpenGraphImageURL.POLICY,
-          width: OPEN_GRAPH_IMAGE_WIDTH,
-          height: OPEN_GRAPH_IMAGE_HEIGHT,
-          alt: OpenGraphImageAlt.POLICY,
-        },
-      ],
-    },
+    // openGraph: { //FIXME: delete or fix this code
+    //   title: MetadataTitle.POLICY,
+    //   description: MetadataDescription.POLICY,
+    //   //   url: OpenGraphURL.POLICY,
+    //   type: OpenGraphType.Article,
+    //   images: [
+    //     {
+    //       //   url: OpenGraphImageURL.POLICY,
+    //       width: OPEN_GRAPH_IMAGE_WIDTH,
+    //       height: OPEN_GRAPH_IMAGE_HEIGHT,
+    //       alt: OpenGraphImageAlt.POLICY,
+    //     },
+    //   ],
+    // },
   },
   notFound: {
     title: MetadataTitle.NOT_FOUND,
     description: MetadataDescription.NOT_FOUND,
     keywords: MetadataKeywords.NOT_FOUND,
-    openGraph: {
-      title: MetadataTitle.NOT_FOUND,
-      description: MetadataDescription.NOT_FOUND,
-      //   url: OpenGraphURL.NOT_FOUND,
-      type: OpenGraphType.Article,
-      images: [
-        {
-          //   url: OpenGraphImageURL.NOT_FOUND,
-          width: OPEN_GRAPH_IMAGE_WIDTH,
-          height: OPEN_GRAPH_IMAGE_HEIGHT,
-          alt: OpenGraphImageAlt.NOT_FOUND,
-        },
-      ],
-    },
+    // openGraph: { //FIXME: delete or fix this code
+    //   title: MetadataTitle.NOT_FOUND,
+    //   description: MetadataDescription.NOT_FOUND,
+    //   //   url: OpenGraphURL.NOT_FOUND,
+    //   type: OpenGraphType.Article,
+    //   images: [
+    //     {
+    //       //   url: OpenGraphImageURL.NOT_FOUND,
+    //       width: OPEN_GRAPH_IMAGE_WIDTH,
+    //       height: OPEN_GRAPH_IMAGE_HEIGHT,
+    //       alt: OpenGraphImageAlt.NOT_FOUND,
+    //     },
+    //   ],
+    // },
   },
   offer: {
     title: MetadataTitle.OFFER,
     description: MetadataDescription.OFFER,
     keywords: MetadataKeywords.OFFER,
-    openGraph: {
-      title: MetadataTitle.OFFER,
-      description: MetadataDescription.OFFER,
-      //   url: OpenGraphURL.OFFER,
-      type: OpenGraphType.Article,
-      images: [
-        {
-          //   url: OpenGraphImageURL.OFFER,
-          width: OPEN_GRAPH_IMAGE_WIDTH,
-          height: OPEN_GRAPH_IMAGE_HEIGHT,
-          alt: OpenGraphImageAlt.OFFER,
-        },
-      ],
-    },
+    // openGraph: { //FIXME: delete or fix this code
+    //   title: MetadataTitle.OFFER,
+    //   description: MetadataDescription.OFFER,
+    //   //   url: OpenGraphURL.OFFER,
+    //   type: OpenGraphType.Article,
+    //   images: [
+    //     {
+    //       //   url: OpenGraphImageURL.OFFER,
+    //       width: OPEN_GRAPH_IMAGE_WIDTH,
+    //       height: OPEN_GRAPH_IMAGE_HEIGHT,
+    //       alt: OpenGraphImageAlt.OFFER,
+    //     },
+    //   ],
+    // },
   },
 };
