6df2737ebf1807c5c43a43cad48a36ffbec1682a
SyroidNadiia <112964918+SyroidNadiia@users.noreply.github.com>
2024-09-07 14:21:21 +0300

    fix: moved the commented code to the draft

--- e2e_tests/UiScenario.spec.ts
+++ e2e_tests/UiScenario.spec.ts
@@ -14,7 +14,6 @@ test('test', async ({ page }) => {
   // Взаємодія з новими вкладками
   const page1Promise = page.waitForEvent('popup');
   await page.getByRole('banner').getByRole('link', { name: 'Open link to @' }).click();
-  // const page1 = await page1Promise;
   await page1Promise;
 
   const page2Promise = page.waitForEvent('popup');
@@ -25,16 +24,15 @@ test('test', async ({ page }) => {
     })
     .getByRole('link')
     .click();
-  // const page2 = await page2Promise;
   await page2Promise;
 
   // Перехід на інші секції сайту та очікування завантаження
   await page.goto('http://localhost:3000/#services');
-  await page.waitForLoadState('load');
+  await page.waitForNavigation();
   await page.goto('http://localhost:3000/#about-us');
-  await page.waitForLoadState('load');
+  await page.waitForNavigation();
   await page.goto('http://localhost:3000/#feedback');
-  await page.waitForLoadState('load');
+  await page.waitForNavigation();
 
   // Натискання на кнопку "Скрол доверху"
   const scrollButton = page.locator('button[aria-label="Скрол доверху"]');
