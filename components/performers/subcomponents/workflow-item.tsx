071b35a780a005a548f73fe1b054b8c3627c2e55
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2024-09-12 17:21:04 +0300

    fix: bug of assistive technology

--- components/performers/subcomponents/workflow-item.tsx
+++ components/performers/subcomponents/workflow-item.tsx
@@ -14,9 +14,9 @@ export default function WorkflowItem({ count, header, desc, gridMarkup }: IWorkf
       className={`${gridMarkup} lg:span-3 max-md:flex max-md:items-center max-md:gap-x-4 md:w-[456px] md:space-y-4 md:max-lg:even:ml-auto lg:w-[568px]`}
     >
       <div className='w-max rounded-full bg-accentPrimary dark:bg-accentSecondary md:mx-auto'>
-        <p className='flex size-10 items-center justify-center text-medium font-bold text-whiteBase md:size-[46px] md:text-big lg:size-12 lg:text-xl'>
+        <span className='flex size-10 items-center justify-center text-medium font-bold text-whiteBase dark:text-darkBase-light md:size-[46px] md:text-big lg:size-12 lg:text-xl'>
           {count}
-        </p>
+        </span>
       </div>
       <div className='md:text-center'>
         <h3 className='mb-2 text-medium font-bold max-sm:text-base md:text-big lg:text-xl'>
