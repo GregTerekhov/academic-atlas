d280d5f627df79d1e72c4db9ec4298f4d4bb481b
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2024-10-30 22:18:02 +0200

    draft: comment links in Footer

--- data/layoutsData.ts
+++ data/layoutsData.ts
@@ -56,33 +56,33 @@ export const getAdaptedContacts = (variant: PositionInLayout) => {
 };
 
 export const getFooterLinks = (): ILinks[] => {
-  const { Services, AboutUs, Feedback, Overview, Promotions } = getIdValues();
+  // const { Services, AboutUs, Feedback, Overview, Promotions } = getIdValues();
   return [
-    {
-      id: Overview,
-      path: Paths.Overview,
-      label: MenuLinks.Overview,
-    },
-    {
-      id: AboutUs,
-      path: Paths.AboutUs,
-      label: MenuLinks.AboutUs,
-    },
-    {
-      id: Feedback,
-      path: Paths.Feedback,
-      label: MenuLinks.Feedback,
-    },
-    {
-      id: Services,
-      path: Paths.Services,
-      label: MenuLinks.Services,
-    },
-    {
-      id: Promotions,
-      path: Paths.Promotions,
-      label: MenuLinks.Promotions,
-    },
+    // {
+    //   id: Overview,
+    //   path: Paths.Overview,
+    //   label: MenuLinks.Overview,
+    // },
+    // {
+    //   id: AboutUs,
+    //   path: Paths.AboutUs,
+    //   label: MenuLinks.AboutUs,
+    // },
+    // {
+    //   id: Feedback,
+    //   path: Paths.Feedback,
+    //   label: MenuLinks.Feedback,
+    // },
+    // {
+    //   id: Services,
+    //   path: Paths.Services,
+    //   label: MenuLinks.Services,
+    // },
+    // {
+    //   id: Promotions,
+    //   path: Paths.Promotions,
+    //   label: MenuLinks.Promotions,
+    // },
     {
       path: Paths.FAQ,
       label: MenuLinks.FAQ,
