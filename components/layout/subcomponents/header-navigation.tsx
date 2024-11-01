895e06e279938dec205864a50c73e7349feba766
Max3281 <113448092+Max3281@users.noreply.github.com>
2024-11-01 09:53:36 +0300

    uncomment header link

--- components/layout/subcomponents/header-navigation.tsx
+++ components/layout/subcomponents/header-navigation.tsx
@@ -1,45 +1,34 @@
 'use client';
 
-// import Link from 'next/link';
-// import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 import { AriaLabel, PositionInLayout } from 'types';
-// import { useActiveLink } from 'context';
+import { useActiveLink } from 'context';
 import { getAdaptedLinks } from 'data';
-import {
-  // getMenuAriaCurrent,
-  mapArray,
-} from 'helpers';
+import { getMenuAriaCurrent, mapArray } from 'helpers';
 
 import CalculationLinkMobile from './calculation-link-mobile';
 
-// import { getNavigationLinkStyles } from 'styles';
+import { getNavigationLinkStyles } from 'styles';
 
 export default function Navigation() {
-  // const { activatedLink,
-  // handleActivateLink
-
-  //  } = useActiveLink();
-  // const pathname = usePathname();
+  const { activatedLink, handleActivateLink } = useActiveLink();
+  const pathname = usePathname();
 
   const adaptedLinks = getAdaptedLinks();
 
   return (
     <nav aria-label={AriaLabel.Navigation}>
       <ul className='max-lg:space-y-6 lg:flex lg:gap-x-8'>
-        {mapArray(
-          adaptedLinks,
-          ({
-            // path,
-            label,
-          }) => {
-            // const isActive = path === activatedLink;
-            // const ariaCurrent = getMenuAriaCurrent(path, pathname, isActive);
-            // const linkClass = getNavigationLinkStyles(isActive);
+        {mapArray(adaptedLinks, ({ path, label }) => {
+          const isActive = path === activatedLink;
+          const ariaCurrent = getMenuAriaCurrent(path, pathname, isActive);
+          const linkClass = getNavigationLinkStyles(isActive);
 
-            return (
-              <li key={label}>
-                {/* <Link
+          return (
+            <li key={label}>
+              <Link
                 href={path}
                 scroll={true}
                 onClick={() => {
@@ -49,11 +38,10 @@ export default function Navigation() {
                 className={linkClass}
               >
                 {label}
-              </Link> */}
-              </li>
-            );
-          },
-        )}
+              </Link>
+            </li>
+          );
+        })}
         <li className='hidden dark:text-whiteBase max-lg:block'>
           <CalculationLinkMobile position={PositionInLayout.Header} />
         </li>
