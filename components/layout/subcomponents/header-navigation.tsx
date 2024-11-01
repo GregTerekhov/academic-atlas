1f502337b4bfb69441befa82130e9624c617ab5e
Max3281 <113448092+Max3281@users.noreply.github.com>
2024-11-01 14:24:51 +0300

    change link on button. route.push(/offer)

--- components/layout/subcomponents/header-navigation.tsx
+++ components/layout/subcomponents/header-navigation.tsx
@@ -1,38 +1,51 @@
 'use client';
 
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+// import Link from 'next/link';
+import {
+  // usePathname,
+  useRouter,
+} from 'next/navigation';
 
-import { AriaLabel, PositionInLayout } from 'types';
-import { useActiveLink } from 'context';
+import { AriaDescription, AriaId, AriaLabel, PositionInLayout } from 'types';
+// import { useActiveLink } from 'context';
 import { getAdaptedLinks } from 'data';
-import { getMenuAriaCurrent, mapArray } from 'helpers';
+import {
+  // getMenuAriaCurrent,
+  mapArray,
+} from 'helpers';
 
 import CalculationLinkMobile from './calculation-link-mobile';
 
-import { getNavigationLinkStyles } from 'styles';
+// import { getNavigationLinkStyles } from 'styles';
+import { PrimaryButtonUI } from 'ui';
 
 export default function Navigation() {
-  const { activatedLink, handleActivateLink } = useActiveLink();
-  const pathname = usePathname();
+  // const { activatedLink, handleActivateLink } = useActiveLink();
+  // const pathname = usePathname();
 
   const adaptedLinks = getAdaptedLinks();
 
   return (
     <nav aria-label={AriaLabel.Navigation}>
       <ul className='max-lg:space-y-6 lg:flex lg:gap-x-8'>
-        {mapArray(adaptedLinks, ({ path, label }) => {
-          const isActive = path === activatedLink;
-          const ariaCurrent = getMenuAriaCurrent(path, pathname, isActive);
-          const linkClass = getNavigationLinkStyles(isActive);
+        {mapArray(
+          adaptedLinks,
+          ({
+            // path,
+            label,
+          }) => {
+            // const isActive = path === activatedLink;
+            // const ariaCurrent = getMenuAriaCurrent(path, pathname, isActive);
+            // const linkClass = getNavigationLinkStyles(isActive);
 
-          // type pathAsType = '/' | '/faq' | '/offer' | '/partnership';
-          // const altPath: pathAsType = '/partnership';
+            // type pathAsType = '/' | '/faq' | '/offer' | '/partnership';
+            // const altPath: pathAsType = '/partnership';
+            const router = useRouter();
 
-          return (
-            <li key={label}>
-              <Link
-                href={'/offer'}
+            return (
+              <li key={label}>
+                {/* <Link
+                href={'/faq'}
                 scroll={true}
                 onClick={() => {
                   handleActivateLink(path);
@@ -41,10 +54,18 @@ export default function Navigation() {
                 className={linkClass}
               >
                 {label}
-              </Link>
-            </li>
-          );
-        })}
+              </Link> */}
+                <PrimaryButtonUI
+                  ariaId={AriaId.Performers}
+                  ariaDescription={AriaDescription.Performers}
+                  handleClick={() => router.push('/offer')}
+                >
+                  {label}
+                </PrimaryButtonUI>
+              </li>
+            );
+          },
+        )}
         <li className='hidden dark:text-whiteBase max-lg:block'>
           <CalculationLinkMobile position={PositionInLayout.Header} />
         </li>
