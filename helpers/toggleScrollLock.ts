370299130fe2822149f1cfe6bad5f2d3d306c870
SyroidNadiia <112964918+SyroidNadiia@users.noreply.github.com>
2024-11-10 16:46:44 +0200

    fix: conditionally check for window object to prevent ReferenceError

--- helpers/toggleScrollLock.ts
+++ helpers/toggleScrollLock.ts
@@ -25,46 +25,47 @@ export const getScrollBarWidth = () => {
 };
 
 export const hasScrollbar = () => {
-  return typeof document !== 'undefined' && document.body.scrollHeight > document.body.clientHeight;
+  if (typeof document === 'undefined') return false;
+  return document.body.scrollHeight > document.body.clientHeight;
 };
 
 export const toggleScrollLock = (isLocked: boolean) => {
-  if (typeof document !== 'undefined') {
-    const root = document.firstElementChild;
-    const body = document.body;
+  if (typeof document === 'undefined' || typeof window === 'undefined') return;
 
-    if (isLocked) {
-      //Check on iPhone -->
-      const scrollPosition = window.scrollY;
-      sessionStorage.setItem('scrollPosition', scrollPosition.toString());
-      //Check on iPhone <--
+  const root = document.firstElementChild;
+  const body = document.body;
 
-      body.ontouchmove = function (e) {
-        e.preventDefault();
-      };
-      body.style.top = `-${scrollPosition}px`;
-      body.classList.add('no-scroll');
-      root?.classList.add('no-scroll');
+  if (isLocked) {
+    //Check on iPhone -->
+    const scrollPosition = window.scrollY;
+    sessionStorage.setItem('scrollPosition', scrollPosition.toString());
+    //Check on iPhone <--
 
-      if (hasScrollbar() && getScrollBarWidth() !== null) {
-        body.style.width = `calc(100% - ${getScrollBarWidth()}px)`;
-      } else {
-        body.style.width = '100%';
-      }
+    body.ontouchmove = function (e) {
+      e.preventDefault();
+    };
+    body.style.top = `-${scrollPosition}px`;
+    body.classList.add('no-scroll');
+    root?.classList.add('no-scroll');
+
+    if (hasScrollbar() && getScrollBarWidth() !== null) {
+      body.style.width = `calc(100% - ${getScrollBarWidth()}px)`;
     } else {
-      body.ontouchmove = null;
-      body.classList.remove('no-scroll');
-      root?.classList.remove('no-scroll');
-      //Check on iPhone --->
-      const savedScrollPosition = sessionStorage.getItem('scrollPosition');
-      if (savedScrollPosition) {
-        const scrollY = parseInt(savedScrollPosition, 10);
-        sessionStorage.removeItem('scrollPosition');
-        window.scrollTo(0, scrollY);
-      }
-      //Check on iPhone <---
-      body.style.top = '';
-      body.style.width = '';
+      body.style.width = '100%';
     }
+  } else {
+    body.ontouchmove = null;
+    body.classList.remove('no-scroll');
+    root?.classList.remove('no-scroll');
+    //Check on iPhone --->
+    const savedScrollPosition = sessionStorage.getItem('scrollPosition');
+    if (savedScrollPosition) {
+      const scrollY = parseInt(savedScrollPosition, 10);
+      sessionStorage.removeItem('scrollPosition');
+      window.scrollTo(0, scrollY);
+    }
+    //Check on iPhone <---
+    body.style.top = '';
+    body.style.width = '';
   }
 };
